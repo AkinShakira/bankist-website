935abadca0d1bfa8d0a6ae74a88da151058f4ff9
AkinShakira <shakiraakinleye@gmail.com>
2022-08-01 22:50:55 +0100

    Scrolling: smooth scrollling

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const h1 = document.querySelector('h1');
+
+
+// IMPLEMENING SMOOTH SCROLLING
+btnScrollTo.addEventListener('click', function (e) {
+
+  // NEW WAY OF IMPLEMETNING SCROLL
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+
